993b08d57c6c86bebb360d358926b5d8ea637b6f
BigBadAI <flashmefusion@hotmail.ca>
2021-01-13 21:10:37 +0000

    intergrated controls and the exit controls

--- play.js
+++ play.js
@@ -3,6 +3,19 @@ const net = require('net');
 /**
  * Establishes connection with the game server
  */
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+};
+const handleUserInput = function(stdin) {
+  if (stdin === '\u0003') {
+    process.exit();
+  }
+  return stdin;
+};
+
 
 const connect = require('./client');
 console.log('Connecting ...');
